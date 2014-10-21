f3c86a2a1df332e8501d8137757d4f8b3dc644bf
nunberty <kramar.alina@gmail.com>
2014-10-21 02:11:04 +0000

    rewrite proper nouns search

---
+++ src/fb2_to_xml.py
@@ -0,0 +1,8 @@
+# Translate fb2 format to correct xml tree format for parsing
+
+import sys
+
+def translate(filename):
+    print(filename)
+
+translate(sys.argv[1])
--- src/object_detector.py
+++ src/object_detector.py
@@ -1,5 +1,31 @@
 import sys
+import nltk.data
 import xml.etree.ElementTree as ET
+import itertools
+
+class NamedObject(object):
+    def __init__(self):
+        self.names = []
+
+    def is_disjoint(self, another):
+        pass
+
+    def add(self, name):
+        self.names.append(name)
+
+class Person(NamedObject):
+    def __init__(self):
+        NamedObject.__init__(self)
+
+    def __str__(self):
+        print("Person: ".join(str(self.names)))
+
+class Location(NamedObject):
+    def __init__(self):
+        NamedObject.__init__(self)
+
+    def __str__(self):
+        print("Location: ".join(str(self.names)))
 
 def load_dictionary(filename):
     with open(filename) as dict_src:
@@ -8,47 +34,51 @@ def load_dictionary(filename):
     ret = set(data.splitlines())
     return ret
 
-tree = ET.parse("../datasets/1/1.fb2")
+def parse_fb2(source):
+    return ET.parse(source)
 
-# I dislike this code
-sents = []
-for body in tree.find('body'):
-    for p in body.findall('p'):
-        if p.text:
-            for s in p.text.split("."):
-                for l in s.split("?"):
-                    for k in l.split("!"):
-                        sents.append(k.strip())
+def get_enumerate_sents(tree):
+    sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
+    sents = []
+    for body in tree.find('body'):
+        for p in body.findall('p'):
+            if p.text:
+                sents.extend(sent_detector.tokenize(p.text.strip()))
 
-sents = enumerate(sents)
+    return enumerate(sents)
 
-places = load_dictionary("../dictionaries/places.txt")
-professions = load_dictionary("../dictionaries/professions.txt")
+def find_proper(sents):
 
-names = set()
-points = set()
-for s in sents:
+    def find_capitalize_words(tokens):
+        ret = []
+        for t in tokens:
+            for k, g in itertools.groupby(tokens,key = lambda s : s[0].isupper()):
+                if k:
+                    ret.append(tuple(g))
+        return ret;
 
-    def is_proper(word):
-        return word[0].isupper()
+    propers = []
+    for i, sent in sents:
+        tokens = nltk.word_tokenize(sent)
+        propers.extend(find_capitalize_words(tokens[1:]))
+    return list(set(propers))
 
-    words = s[1].split()
-    if len(words) > 1:
-        for pair in zip(words[1:], words[2:]):
-            a = pair[0].strip(",-.':")
-            b = pair[1].strip(",-.':")
-            if len(a) > 1 and len(b) > 1:
+def analyze_propers(propers):
+    return 1, 3
 
-                print(" ".join(t for t in [a, b]))
-                if is_proper(a) and is_proper(b) and 'I' not in (a, b):
+def gether_statistic(data, propers):
+    pass
 
-                    if a.lower() in places or b.lower() in places:
-                        points.add((a, b))
-                    else:
+# Will be script parameter
+filename = "../datasets/1/1.fb2"
 
-                        names.add((a, b))
-                        names.add((a))
-                        names.add((b))
+tree = parse_fb2(filename)
+sents = get_enumerate_sents(tree)
 
-#print(" ".join(str(t) for t in names))
-#print(" ".join(str(p) for p in points))
+places_dictionary = load_dictionary("../dictionaries/places.txt")
+personality_dictionary = load_dictionary("../dictionaries/professions.txt")
+
+propers = sorted(find_proper(sents), key = lambda x : -1 * len(x))
+
+persons, locations = analyze_propers(propers)
+print("\n".join(str(y) for y in propers))
