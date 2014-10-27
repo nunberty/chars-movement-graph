6b4f2466165650c312144a086fb445f54e966b78
nunberty <kramar.alina@gmail.com>
2014-10-27 16:39:51 +0000

    fixes #5 : script replace in file wrong xml (from fb2) to correct

--- src/fb2_to_xml.py
+++ src/fb2_to_xml.py
@@ -1,8 +1,21 @@
 # Translate fb2 format to correct xml tree format for parsing
-
 import sys
 
-def translate(filename):
-    print(filename)
+def translate(data):
+    data = data.split("xmlns")
+    ret = data[0]
+    for part in data[1:]:
+        if not part[0] == ":":
+            ret += "xmlns:xhtml" + part
+        else:
+            ret += "xmlns" + part
+    return ret
 
-translate(sys.argv[1])
+if __name__ == "__main__":
+    filename = sys.argv[1]
+    with open(filename) as wrong_fb2:
+        data = wrong_fb2.read()
+
+    correct_data = translate(data)
+    with open(filename, "w") as correct_fb2:
+            correct_fb2.write(correct_data)
