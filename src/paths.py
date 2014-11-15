ed63a5970a9fd0cdfe08e3cf66cc2efe38eec528
nunberty <kramar.alina@gmail.com>
2014-11-15 17:14:37 +0000

    add related paths with prefix

--- src/paths.py
+++ src/paths.py
@@ -1,8 +1,10 @@
-ds1="../datasets/1/1.fb2"
-ds2="../datasets/2/2.fb2"
-ds3="../datasets/3/3.fb2"
-places_path="../dictionaries/places.txt"
-profs_path="../dictionaries/professions.txt"
-preps_path="../dictionaries/directionprep.txt"
-stop_path="../dictionaries/stopwords.txt"
-english_pickle="tokenizers/punkt/english.pickle"
+from local_path import prefix
+
+ds1 = prefix + "/datasets/1/1.fb2"
+ds2 = prefix + "/datasets/2/2.fb2"
+ds3 = prefix + "/datasets/3/3.fb2"
+places_path = prefix + "/dictionaries/places.txt"
+profs_path = prefix + "/dictionaries/professions.txt"
+preps_path = prefix + "/dictionaries/directionprep.txt"
+stop_path = prefix + "/dictionaries/stopwords.txt"
+english_pickle = prefix + "tokenizers/punkt/english.pickle"
